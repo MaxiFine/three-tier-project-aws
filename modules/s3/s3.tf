40e329a99684a4e27be7f672e66bcafb9f41c9aa
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-26 15:33:16 +0000

    s3 fixes

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -26,134 +26,134 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
 # }
 
 
-##################
-## S3 DATA REPLICATION
-# provider "aws" {
-#   region = "eu-west-1"
+# ##################
+# ## S3 DATA REPLICATION
+# # provider "aws" {
+# #   region = "eu-west-1"
+# # }
+
+# # provider "aws" {
+# #   alias  = "central"
+# #   region = "eu-central-1"
+# # }
+
+# data "aws_iam_policy_document" "assume_role" {
+#   statement {
+#     effect = "Allow"
+
+#     principals {
+#       type        = "Service"
+#       identifiers = ["s3.amazonaws.com"]
+#     }
+
+#     actions = ["sts:AssumeRole"]
+#   }
 # }
 
-# provider "aws" {
-#   alias  = "central"
-#   region = "eu-central-1"
+# resource "aws_iam_role" "replication" {
+#   name               = "tf-iam-role-replication-12345"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 # }
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "replication" {
+#   statement {
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
+#     actions = [
+#       "s3:GetReplicationConfiguration",
+#       "s3:ListBucket",
+#     ]
 
-    actions = ["sts:AssumeRole"]
-  }
-}
+#     resources = [aws_s3_bucket.source.arn]
+#   }
 
-resource "aws_iam_role" "replication" {
-  name               = "tf-iam-role-replication-12345"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
+#   statement {
+#     effect = "Allow"
 
-data "aws_iam_policy_document" "replication" {
-  statement {
-    effect = "Allow"
+#     actions = [
+#       "s3:GetObjectVersionForReplication",
+#       "s3:GetObjectVersionAcl",
+#       "s3:GetObjectVersionTagging",
+#     ]
 
-    actions = [
-      "s3:GetReplicationConfiguration",
-      "s3:ListBucket",
-    ]
+#     resources = ["${aws_s3_bucket.source.arn}/*"]
+#   }
 
-    resources = [aws_s3_bucket.source.arn]
-  }
+#   statement {
+#     effect = "Allow"
 
-  statement {
-    effect = "Allow"
+#     actions = [
+#       "s3:ReplicateObject",
+#       "s3:ReplicateDelete",
+#       "s3:ReplicateTags",
+#     ]
 
-    actions = [
-      "s3:GetObjectVersionForReplication",
-      "s3:GetObjectVersionAcl",
-      "s3:GetObjectVersionTagging",
-    ]
+#     resources = ["${aws_s3_bucket.destination.arn}/*"]
+#   }
+# }
 
-    resources = ["${aws_s3_bucket.source.arn}/*"]
-  }
+# resource "aws_iam_policy" "replication" {
+#   name   = "tf-iam-role-policy-replication-12345"
+#   policy = data.aws_iam_policy_document.replication.json
+# }
 
-  statement {
-    effect = "Allow"
+# resource "aws_iam_role_policy_attachment" "replication" {
+#   role       = aws_iam_role.replication.name
+#   policy_arn = aws_iam_policy.replication.arn
+# }
 
-    actions = [
-      "s3:ReplicateObject",
-      "s3:ReplicateDelete",
-      "s3:ReplicateTags",
-    ]
+# resource "aws_s3_bucket" "destination" {
+#   bucket = "tf-test-bucket-destination-12345"
+# }
 
-    resources = ["${aws_s3_bucket.destination.arn}/*"]
-  }
-}
+# resource "aws_s3_bucket_versioning" "destination" {
+#   bucket = aws_s3_bucket.destination.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_iam_policy" "replication" {
-  name   = "tf-iam-role-policy-replication-12345"
-  policy = data.aws_iam_policy_document.replication.json
-}
+# resource "aws_s3_bucket" "source" {
+#   provider = aws.central
+#   bucket   = "tf-test-bucket-source-12345"
+# }
 
-resource "aws_iam_role_policy_attachment" "replication" {
-  role       = aws_iam_role.replication.name
-  policy_arn = aws_iam_policy.replication.arn
-}
+# resource "aws_s3_bucket_acl" "source_bucket_acl" {
+#   provider = aws.central
 
-resource "aws_s3_bucket" "destination" {
-  bucket = "tf-test-bucket-destination-12345"
-}
+#   bucket = aws_s3_bucket.source.id
+#   acl    = "private"
+# }
 
-resource "aws_s3_bucket_versioning" "destination" {
-  bucket = aws_s3_bucket.destination.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "source" {
+#   provider = aws.central
 
-resource "aws_s3_bucket" "source" {
-  provider = aws.central
-  bucket   = "tf-test-bucket-source-12345"
-}
+#   bucket = aws_s3_bucket.source.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_acl" "source_bucket_acl" {
-  provider = aws.central
+# resource "aws_s3_bucket_replication_configuration" "replication" {
+#   provider = aws.central
+#   # Must have bucket versioning enabled first
+#   depends_on = [aws_s3_bucket_versioning.source]
 
-  bucket = aws_s3_bucket.source.id
-  acl    = "private"
-}
+#   role   = aws_iam_role.replication.arn
+#   bucket = aws_s3_bucket.source.id
 
-resource "aws_s3_bucket_versioning" "source" {
-  provider = aws.central
+#   rule {
+#     id = "foobar"
 
-  bucket = aws_s3_bucket.source.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#     filter {
+#       prefix = "foo"
+#     }
 
-resource "aws_s3_bucket_replication_configuration" "replication" {
-  provider = aws.central
-  # Must have bucket versioning enabled first
-  depends_on = [aws_s3_bucket_versioning.source]
+#     status = "Enabled"
 
-  role   = aws_iam_role.replication.arn
-  bucket = aws_s3_bucket.source.id
-
-  rule {
-    id = "foobar"
-
-    filter {
-      prefix = "foo"
-    }
-
-    status = "Enabled"
-
-    destination {
-      bucket        = aws_s3_bucket.destination.arn
-      storage_class = "STANDARD"
-    }
-  }
-}
+#     destination {
+#       bucket        = aws_s3_bucket.destination.arn
+#       storage_class = "STANDARD"
+#     }
+#   }
+# }
