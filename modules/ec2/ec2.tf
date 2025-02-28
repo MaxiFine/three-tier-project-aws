3ddc543f45c9867bdcf191b0671ba8b8181df709
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-28 01:20:12 +0000

    ec2 sg resolving

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -10,12 +10,18 @@ resource "aws_instance" "PublicWebTemplat-1" {
   instance_type = "t2.micro"
   # subnet_id     = aws_subnet.public-web-subnet-1.id
   subnet_id = var.public_web_subnet_1
+  key_name = "vagrant-key"
   # security_groups = [ var.web_security_group ]
   # user_data = base64encode(data.index_file.user_data.rendered)
   # user_data     = base64encode(templatefile("${path.module}/user_data.sh", {}))
-  user_data = base64encode(templatefile("${path.root}/user_data.sh", {}))
+  # user_data = base64encode(templatefile("${path.root}/user_data.sh", {}))
   # vpc_security_group_ids = [var.web_security_group]
   # vpc_security_group_ids = []
+   user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World 1" > index.html
+              python3 -m http.server 8080 &
+              EOF
   security_groups = [var.web_security_group]
 
 
@@ -25,26 +31,32 @@ resource "aws_instance" "PublicWebTemplat-1" {
   }
 }
 
-resource "aws_instance" "PublicWebTemplate-2" {
-  ami           = "ami-03fd334507439f4d1" # Replace with your AMI ID
-  instance_type = "t2.micro"
-  # subnet_id     = aws_subnet.public-web-subnet-2.id
-  # subnet_id = var.public_web_subnet_2
-  subnet_id = var.private_app_subnet_2
-  # security_groups = [ var.web_security_group ]
-  # vpc_security_group_ids = [ var.web_security_group ]
-  # vpc_security_group_ids = [ ]
-  security_groups = [var.web_security_group]
+# resource "aws_instance" "PublicWebTemplate-2" {
+#   ami           = "ami-03fd334507439f4d1" # Replace with your AMI ID
+#   instance_type = "t2.micro"
+#   # subnet_id     = aws_subnet.public-web-subnet-2.id
+#   # subnet_id = var.public_web_subnet_2
+#   subnet_id = var.private_app_subnet_2
+#   # security_groups = [ var.web_security_group ]
+#   # vpc_security_group_ids = [ var.web_security_group ]
+#   # vpc_security_group_ids = [ ]
+#   security_groups = [var.web_security_group]
+#   key_name = "vagrant-key"
 
-  # user_data = base64encode(data.index_file.user_data.rendered)
-  # user_data     = base64encode(templatefile("${path.module}/user_data.sh", {}))
-  user_data = base64encode(templatefile("${path.root}/user_data.sh", {}))
+#   # user_data = base64encode(data.index_file.user_data.rendered)
+#   # user_data     = base64encode(templatefile("${path.module}/user_data.sh", {}))
+#   # user_data = base64encode(templatefile("${path.root}/user_data.sh", {}))
+#   user_data   = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World 1" > index.html
+#               python3 -m http.server 8080 &
+#               EOF
 
 
-  tags = {
-    Name = "PublicWebTemplate-2"
-  }
-}
+#   tags = {
+#     Name = "PublicWebTemplate-2"
+#   }
+# }
 
 # ADD APP TIER INSTANCES
 resource "aws_instance" "PrivateAppTemplat-1" {
@@ -55,11 +67,18 @@ resource "aws_instance" "PrivateAppTemplat-1" {
   # security_groups = [ var.app_security_group ]
   # vpc_security_group_ids = [ ]
   security_groups = [var.app_security_group]
+  key_name = "vagrant-key"
+
   # vpc_security_group_ids = [ var.app_security_group ]
 
   # user_data = base64encode(data.index_file.user_data.rendered)
   # user_data     = base64encode(templatefile("${path.module}/user_data.sh", {}))
-  user_data = base64encode(templatefile("${path.root}/user_data.sh", {}))
+  # user_data = base64encode(templatefile("${path.root}/user_data.sh", {}))
+  user_data =  <<-EOF
+              #!/bin/bash
+              echo "Hello, World 1" > index.html
+              python3 -m http.server 8080 &
+              EOF
 
 
   tags = {
@@ -67,23 +86,24 @@ resource "aws_instance" "PrivateAppTemplat-1" {
   }
 }
 
-resource "aws_instance" "PrivateAppTemplate-2" {
-  ami           = "ami-03fd334507439f4d1" # Replace with your AMI ID
-  instance_type = "t2.micro"
-  # subnet_id     = aws_subnet.private-app-subnet-2.id
-  subnet_id = var.private_app_subnet_2
-  # security_groups = [ var.app_security_group ]
-  # vpc_security_group_ids = [ var.app_security_group]
-  # vpc_security_group_ids = []
-  security_groups = [var.app_security_group]
+# resource "aws_instance" "PrivateAppTemplate-2" {
+#   ami           = "ami-03fd334507439f4d1" # Replace with your AMI ID
+#   instance_type = "t2.micro"
+#   # subnet_id     = aws_subnet.private-app-subnet-2.id
+#   subnet_id = var.private_app_subnet_2
+#   # security_groups = [ var.app_security_group ]
+#   # vpc_security_group_ids = [ var.app_security_group]
+#   # vpc_security_group_ids = []
+#   key_name = "vagrant-key"
+#   security_groups = [var.app_security_group]
 
-  # user_data = base64encode(data.index_file.user_data.rendered)
-  # user_data     = base64encode(templatefile("${path.module}/user_data.sh", {}))
-  user_data = base64encode(templatefile("${path.root}/user_data.sh", {}))
+#   # user_data = base64encode(data.index_file.user_data.rendered)
+#   # user_data     = base64encode(templatefile("${path.module}/user_data.sh", {}))
+#   user_data = base64encode(templatefile("${path.root}/user_data.sh", {}))
 
 
 
-  tags = {
-    Name = "PrivateAppTemplate-2"
-  }
-}
+#   tags = {
+#     Name = "PrivateAppTemplate-2"
+#   }
+# }
