f647aabbfc60049a4cc95098d25750565b6d58d0
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-03-03 02:24:42 +0000

    fix rds sg error

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,202 +1,23 @@
-# provider "aws" {
-#   region = local.origin_region
-
-#   # Make it faster by skipping something
-#   skip_metadata_api_check     = true
-#   skip_region_validation      = true
-#   skip_credentials_validation = true
-# }
-
-# provider "aws" {
-#   region = local.replica_region
-
-#   alias = "replica"
-
-#   # Make it faster by skipping something
-#   skip_metadata_api_check     = true
-#   skip_region_validation      = true
-#   skip_credentials_validation = true
-# }
-
-# locals {
-#   bucket_name             = "source-s3-bucket-${random_pet.this.id}"
-#   destination_bucket_name = "destination-s3-bucket-${random_pet.this.id}"
-#   origin_region           = "eu-west-1"
-#   replica_region          = "eu-central-1"
-# }
-
-# # data "aws_caller_identity" "current" {}
-
-# resource "random_pet" "this" {
-#   length = 2
-# }
-
-# resource "aws_kms_key" "replica" {
-#   provider = aws.replica
-
-#   description             = "S3 bucket replication KMS key"
-#   deletion_window_in_days = 7
-# }
-
-# module "replica_bucket" {
-#   source = "../.."
-
-#   providers = {
-#     aws = aws.replica
-#   }
-
-# #   bucket_name = local.destination_bucket_name
-# }
-
-# module "s3_bucket" {
-#   source = "../../"
-
-#   bucket = local.bucket_name
-
-#   versioning {
-#     enabled = true
-#   }
-
-#   replication_configuration {
-#     role = aws_iam_role.replication.arn
-
-#     rules {
-#       id     = "something-with-kms-and-filter"
-#       status = "Enabled"
-#       priority = 10
-
-#       delete_marker_replication {
-#         status = "Disabled"
-#       }
-
-#       source_selection_criteria {
-#         replica_modifications {
-#           status = "Enabled"
-#         }
-#         sse_kms_encrypted_objects {
-#           status = "Enabled"
-#         }
-#       }
-
-#       filter {
-#         prefix = "one"
-#         tags = {
-#           ReplicateMe = "Yes"
-#         }
-#       }
-
-#       destination {
-#         bucket        = "arn:aws:s3:::${local.destination_bucket_name}"
-#         storage_class = "STANDARD"
-
-#         replica_kms_key_id = aws_kms_key.replica.arn
-#         account_id         = data.aws_caller_identity.current.account_id
-
-#         access_control_translation {
-#           owner = "Destination"
-#         }
-
-#         replication_time {
-#           status = "Enabled"
-#           time {
-#             minutes = 15
-#           }
-#         }
-
-#         metrics {
-#           status = "Enabled"
-#           event_threshold {
-#             minutes = 15
-#           }
-#         }
-#       }
-#     }
-
-#     rules {
-#       id     = "something-with-filter"
-#       status = "Enabled"
-#       priority = 20
-
-#       delete_marker_replication {
-#         status = "Disabled"
-#       }
-
-#       filter {
-#         prefix = "two"
-#         tags = {
-#           ReplicateMe = "Yes"
-#         }
-#       }
-
-#       destination {
-#         bucket        = "arn:aws:s3:::${local.destination_bucket_name}"
-#         storage_class = "STANDARD"
-#       }
-#     }
-
-#     rules {
-#       id     = "everything-with-filter"
-#       status = "Enabled"
-#       priority = 30
-
-#       delete_marker_replication {
-#         status = "Enabled"
-#       }
-
-#       filter {
-#         prefix = ""
-#       }
-
-#       destination {
-#         bucket        = "arn:aws:s3:::${local.destination_bucket_name}"
-#         storage_class = "STANDARD"
-#       }
-#     }
-
-#     rules {
-#       id     = "everything-without-filters"
-#       status = "Enabled"
-
-#       delete_marker_replication {
-#         status = "Enabled"
-#       }
-
-#       destination {
-#         bucket        = "arn:aws:s3:::${local.destination_bucket_name}"
-#         storage_class = "STANDARD"
-#       }
-#     }
-#   }
-# }
-
-###################
-## new fix testing
-###################
-
 provider "aws" {
-  region = "eu-west-1"  # Origin region (e.g., Ireland)
-
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_credentials_validation = true
+  region = "eu-west-1"  # Origin region (Ireland)
 }
 
 provider "aws" {
   alias   = "replica"
-  region  = "eu-central-1"  # Replica region (e.g., Frankfurt)
+  region  = "eu-central-1"  # Replica region (Frankfurt)
+}
 
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_credentials_validation = true
+# Fetch AWS Account ID
+data "aws_caller_identity" "current" {}
+
+# Generate random names for S3 buckets
+resource "random_pet" "this" {
+  length = 2
 }
 
 locals {
-  origin_bucket_name   = "origin-bucket-${random_pet.this.id}"
-  replica_bucket_name  = "replica-bucket-${random_pet.this.id}"
-}
-
-resource "random_pet" "this" {
-  length = 2
+  origin_bucket_name  = "origin-bucket-${random_pet.this.id}"
+  replica_bucket_name = "replica-bucket-${random_pet.this.id}"
 }
 
 #---------------------------
@@ -205,9 +26,13 @@ resource "random_pet" "this" {
 resource "aws_s3_bucket" "replica" {
   provider = aws.replica
   bucket   = local.replica_bucket_name
+}
 
-  versioning {
-    enabled = true  # Required for replication
+resource "aws_s3_bucket_versioning" "replica_versioning" {
+  provider = aws.replica
+  bucket   = aws_s3_bucket.replica.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
@@ -216,45 +41,36 @@ resource "aws_s3_bucket" "replica" {
 #---------------------------
 resource "aws_s3_bucket" "origin" {
   bucket = local.origin_bucket_name
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "origin_versioning" {
+  bucket = aws_s3_bucket.origin.id
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  replication_configuration {
-    role = aws_iam_role.replication.arn
+resource "aws_s3_bucket_replication_configuration" "origin_replication" {
+  bucket = aws_s3_bucket.origin.id
+  role   = aws_iam_role.replication.arn
 
-    rules {
-      id       = "full-replication"
-      status   = "Enabled"
-      priority = 10
+  depends_on = [ aws_s3_bucket_versioning.replica_versioning ]
 
-      filter {}  # Replicate all objects
+  rule {
+    id       = "full-replication"
+    status   = "Enabled"
+    priority = 10
 
-      destination {
-        bucket        = aws_s3_bucket.replica.arn
-        storage_class = "STANDARD"
-      }
+    destination {
+      bucket        = aws_s3_bucket.replica.arn
+      storage_class = "STANDARD"
     }
   }
-
-#   depends_on = [aws_iam_role_policy_attachment.replication]
-  depends_on = [iam.aws_iam_role_policy_attachment.replication]
 }
 
-# ---------------------------
-# KMS Key (Replica Region)
-# ---------------------------
-resource "aws_kms_key" "replica" {
-  provider = aws.replica
-
-  description             = "S3 Replica Bucket KMS Key"
-  deletion_window_in_days = 7
-}
-
-# ---------------------------
-# IAM Role & Policy
-# ---------------------------
+#---------------------------
+# IAM Role & Policy for Replication
+#---------------------------
 resource "aws_iam_role" "replication" {
   name = "s3-replication-role"
 
@@ -286,17 +102,93 @@ resource "aws_iam_policy" "replication" {
           "s3:ReplicateTags"
         ],
         Effect   = "Allow",
-        Resource = "*"
+        Resource = [
+          aws_s3_bucket.origin.arn,
+          "${aws_s3_bucket.origin.arn}/*"
+        ]
+      },
+      {
+        Action = [
+          "s3:ReplicateObject",
+          "s3:ReplicateDelete",
+          "s3:ReplicateTags"
+        ],
+        Effect   = "Allow",
+        Resource = [
+          aws_s3_bucket.replica.arn,
+          "${aws_s3_bucket.replica.arn}/*"
+        ]
       }
     ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "replication" {
-#   role       = aws_iam_role.replication.name
-  role       = aws_iam_role.replication.named 
+  role       = aws_iam_role.replication.name
   policy_arn = aws_iam_policy.replication.arn
 }
 
+# ---------------------------
+# KMS Key (Replica Region)
+# ---------------------------
+resource "aws_kms_key" "replica" {
+  provider = aws.replica
+  description             = "S3 Replica Bucket KMS Key"
+  deletion_window_in_days = 7
 
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "s3-replication-key",
+    Statement = [
+      {
+        Sid    = "EnableReplicationRole",
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_iam_role.replication.arn
+        },
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:GenerateDataKey*"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowRootAccess",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action = "kms:*",
+        Resource = "*"
+      }
+    ]
+  })
+}
 
+# ---------------------------
+# Attach KMS Policy to Replication IAM Role
+# ---------------------------
+resource "aws_iam_policy" "kms_replication" {
+  name = "kms-replication-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "kms:Decrypt",
+          "kms:Encrypt",
+          "kms:GenerateDataKey*"
+        ],
+        Effect   = "Allow",
+        Resource = aws_kms_key.replica.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "kms_replication" {
+  role       = aws_iam_role.replication.name
+  policy_arn = aws_iam_policy.kms_replication.arn
+}
