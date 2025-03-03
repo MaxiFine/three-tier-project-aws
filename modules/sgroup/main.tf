b1cd1e07b7c5f4d28459806d36ee916c4a1bbf2d
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-03-03 02:26:35 +0000

    fix sgroup resolved

--- modules/sgroup/main.tf
+++ modules/sgroup/main.tf
@@ -232,6 +232,13 @@ resource "aws_security_group" "database_security_group" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
 
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "Database Security Group"
   }
