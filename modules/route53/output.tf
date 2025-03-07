9baecdd7f3f6a93d83861cca08af423f02478df4
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-03-07 09:51:04 +0000

    initial r53 configs

--- modules/route53/output.tf
+++ modules/route53/output.tf
@@ -1,22 +1,22 @@
-output "zone_id" {
-  value = aws_route53_zone.main.id
-}
+# output "zone_id" {
+#   value = aws_route53_zone.main.id
+# }
 
 
-output "zone_arn" {
-  value = aws_route53_zone.main.arn
-}
+# output "zone_arn" {
+#   value = aws_route53_zone.main.arn
+# }
 
 
-output "zone_name" {
-  value = aws_route53_zone.main.name
-}
+# output "zone_name" {
+#   value = aws_route53_zone.main.name
+# }
 
 
-output "primaryhealthcheck_id"  {
-    value = aws_route53_health_check.primary.id
+# output "primaryhealthcheck_id"  {
+#     value = aws_route53_health_check.primary.id
   
-}
-output "primaryhealthcheck_arn"  {
-    value = aws_route53_health_check.primary.arn
-}
+# }
+# output "primaryhealthcheck_arn"  {
+#     value = aws_route53_health_check.primary.arn
+# }
