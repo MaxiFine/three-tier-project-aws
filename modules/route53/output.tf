a26eb7cf396e029d9015df53665955ecdfbc74f3
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-03-09 00:48:12 +0000

    resolving route53 attributes in main

--- modules/route53/output.tf
+++ modules/route53/output.tf
@@ -1,16 +1,16 @@
-output "zone_id" {
-  value = aws_route53_zone.main.id
-}
+# output "zone_id" {
+#   value = aws_route53_zone.main.id
+# }
 
 
-output "zone_arn" {
-  value = aws_route53_zone.main.arn
-}
+# output "zone_arn" {
+#   value = aws_route53_zone.main.arn
+# }
 
 
-output "zone_name" {
-  value = aws_route53_zone.main.name
-}
+# output "zone_name" {
+#   value = aws_route53_zone.main.name
+# }
 
 
 output "primaryhealthcheck_id"  {
